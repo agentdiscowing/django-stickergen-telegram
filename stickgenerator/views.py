cf8ff8f7577c159d38a7a0a0997282636c6f82a2
Lolita Hevondian <agentdiscowing@gmail.com>
2020-05-17 00:27:29 +0300

    Managed view for edit page

--- stickgenerator/views.py
+++ stickgenerator/views.py
@@ -1,15 +1,15 @@
 from django.shortcuts import render
+from .effects import effect
+from .effects import add_effect
+from .effects import reset
 from django.http import HttpResponse
 from .models import Image
+from .models import Effect
 # Create your views here.
 
 
 def home_view(request, *args, **kwargs):
-    context = {}
-    if request.method == 'POST':
-        new_image = request.FILES['image']
-        Image.objects.create(name='unknown', image=new_image)
-    return render(request, 'home.html', context)
+    return render(request, 'home.html')
     # last argument is a dict of custom values (context)
 
 
@@ -19,3 +19,29 @@ def stats_view(request, *args, **kwargs):
 
 def faq_view(request, *args, **kwargs):
     return render(request, 'faq.html')
+
+
+def edit_view(request, *args, **kwargs):
+    db_effects = Effect.objects.all()
+    if request.method == 'POST':
+        new_image = request.FILES['image']
+        im = Image.objects.create(image=new_image)
+        context = {
+            'image': im,
+            'effects': db_effects
+        }
+        return render(request, 'edit.html', context)
+    if request.method == 'GET':
+        eff = request.GET.get('effect', '')
+        image_id = request.GET.get('image_id', '')
+        im = Image.objects.get(id=int(image_id))
+        if eff != "Reset":
+            applied_effect = effect[eff]
+            add_effect(applied_effect, im.draft.path)
+        else:
+            reset(im.draft.path, im.image.path)
+        context = {
+            'image': im,
+            'effects': db_effects,
+        }
+        return render(request, 'edit.html', context)
